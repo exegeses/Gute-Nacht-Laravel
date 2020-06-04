773d5317b1f4cb1a1692bbb75fce8474a9b3a450
marcos pinardi <exegeses@gmail.com>
2020-06-04 19:16:26 -0300

    lista regiones

--- proyecto/resources/views/regiones.blade.php
+++ proyecto/resources/views/regiones.blade.php
@@ -4,7 +4,7 @@
     <h1>listado de regiones</h1>
 
     @foreach( $regiones as $region )
-    <div class="alert alert-light col-3 mx-auto">
+    <div class="alert alert-light col-4 mx-auto">
         {{ $region->regNombre }} - id: {{ $region->regID }}
     </div>
     @endforeach
