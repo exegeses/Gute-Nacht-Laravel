57db39e7a62887d7d2db741b0ed0a35cbb6815ca
marcos pinardi <exegeses@gmail.com>
2020-06-04 19:35:52 -0300

    Panel de regiones

---
+++ proyecto/resources/views/adminRegiones.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.plantilla')
+    @section('contenido')
+        <h1>Panel de administración de regiones</h1>
+
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Id</th>
+                    <th>Región</th>
+                    <th colspan="2">
+                        <a href="" class="btn btn-dark">
+                            Agregar
+                        </a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>#</td>
+                    <td>region</td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary">
+                            Modificar
+                        </a>
+                    </td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary">
+                            Eliminar
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+    @endsection
--- proyecto/routes/web.php
+++ proyecto/routes/web.php
@@ -15,32 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 #Route::metodo('/petición', 'acción');
-Route::get('/saludo', function (){
-    $nombre = 'Marcos';
-    $numero = 3;
-    return view('saludo',
-            [
-                'nombre' => $nombre,
-                'numero' => $numero
-            ] );
-});
-
-Route::view('/test', 'vista');
-
-##retornar vista de un formulario
-Route::view('/form', 'formulario');
-// procesar dato desde el form
-Route::get('/proceso', function(){
-    //capturar dato desde el form
-    $nombre = $_GET['nombre'];
-
-    //retornar la vista que mostrar ese dato
-    return view('proceso', [ 'nombre'=>$nombre ] );
-});
 
 ###################################
 ##### consulta con raw SQL
 Route::get('/regiones', function (){
     $regiones = DB::select('SELECT regID, regNombre FROM regiones');
     return view('regiones', ['regiones'=>$regiones]);
+});
+
+################################
+##### CRUD de Regiones
+Route::get('/adminRegiones', function (){
+    return view('adminRegiones');
 });
